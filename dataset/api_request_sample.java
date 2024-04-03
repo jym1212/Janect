78c571b60c9d86fa2af0c59e6fc5e93afc7dc619
SeungwonLee <97718938+Heisnotanimposter@users.noreply.github.com>
2024-04-03 12:00:22 +0900

    update
    update

--- dataset/api_request_sample.java
+++ dataset/api_request_sample.java
@@ -20,7 +20,9 @@ public class ApiExplorer {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
+
         System.out.println("Response code: " + conn.getResponseCode());
+        
         BufferedReader rd;
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
