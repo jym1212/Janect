3bad3deb0515d53ae096ede1d3988f4cefc6d59d
SeungwonLee <97718938+Heisnotanimposter@users.noreply.github.com>
2024-05-03 11:21:35 +0900

    on time update

---
+++ dataset/API/test2/jsonstructrueclass.java
@@ -0,0 +1,12 @@
+public class Certificates {
+    private String examtitle;
+    private String date;
+    private String rateofpass;
+    private Integer exampasser;
+    private String examtitle;
+    private String date;
+    private String rateofpass;
+    private Integer exampasser;
+
+    // Getters and setters
+}
